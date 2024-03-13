94b73991dec12933f3c12a5cb5c4bac1c4e56970
skonk <anntvfamily@gmail.com>
2024-03-13 14:33:38 -0700

    functionality

--- dihybruid.py
+++ dihybruid.py
@@ -19,9 +19,13 @@ parent2list.append(parent2geno[1] + parent2geno[2])
 parent2list.append(parent2geno[1] + parent2geno[3])
 
 punnettsquarelist = []
-for i in range(16):
-    punnettsquarelist.append()
-
 for x in range(4):
     for y in range(4):
-        genotype = parent1list[x][0] + parent2list[y][0]
+        genotype = parent1list[x][0] + parent2list[y][0] + parent1list[x][1] + parent2list[y][1]
+        punnettsquarelist.append(genotype)
+
+for i in range(4):
+    row = []
+    for j in range(4):
+        row.append(punnettsquarelist[(i*4)+j])
+    print(row)
